39c293b1fabaa48c9ba1044075a6a75a645d1270
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2022-01-14 10:54:16 +0000

    Switch from root being /submissions
    This is no longer appropriate because we need to be able to have routes
    below this, such as /requests. So we now redirect requests to / to
    go to /submissions, thus enabling /submissions to be the default route,
    but not precluding others

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import 'regenerator-runtime/runtime';
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { hot } from 'react-hot-loader';
 
 import { Submissions } from './Submissions';
 import Settings from './settings';
-import { SUBMISSIONS_ROUTE } from './common/constants';
+import {
+  ROOT,
+  SUBMISSIONS_ROUTE,
+  REQUESTS_ROUTE
+} from './common/constants';
 
 class Ill extends React.Component {
   static propTypes = {
@@ -27,6 +31,12 @@ class Ill extends React.Component {
     }
     return (
       <Switch>
+        <Route
+          exact
+          path={ROOT}
+        >
+          <Redirect to={{ pathname: SUBMISSIONS_ROUTE }} />
+        </Route>
         <Route
           path={SUBMISSIONS_ROUTE}
           component={Submissions}
